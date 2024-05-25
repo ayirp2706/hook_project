d11c07e261648a5b992833af5cd0f27a4eec6634
ayirp2706 <158231883+ayirp2706@users.noreply.github.com>
2024-05-25 14:39:07 +0530

    Add files via upload

---
+++ froloop.js
@@ -0,0 +1,43 @@
+/*for(basket= 1; basket<5; basket++){
+
+    console.log("basket");
+}
+
+for(number=1; number<20; number++){
+
+    console.log("start")
+}*/
+
+/*for (number=2; number<10;number++ ) {
+
+    console.log("answer")
+}*/
+
+
+/*let amount = 1000
+console.log("your amount is"+amount)
+
+// let name = prompt("hello this your login")
+// console.log('yes'+name)
+
+let a = prompt("enter the number")
+
+console.log(Number(a) +10)*/
+
+
+/*let basket = "let's eat ,"
+let basket1="its on me "
+
+
+let firstName = "priya"
+let lastName ="dharshini"
+let city = "chidhabaram"
+ 
+let place=`${firstName} ${lastName} lives near ${city}`
+
+console.log(place)*/
+
+let  array = [apple,orange,mango,slice]
+
+console.log(array)
+//console.log(array)
